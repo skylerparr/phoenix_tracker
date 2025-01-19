7c1fcc89129815cbe87e391f2a316b73e696efe1
Skyler Parrti <skylerparrti@gmail.com>
2025-01-19 01:03:20 +0000

    more epic warmth

--- frontend/src/components/EpicsComponent.tsx
+++ frontend/src/components/EpicsComponent.tsx
@@ -2,18 +2,28 @@ import React, { useEffect, useState } from "react";
 import { Box, Typography, TextField, IconButton, Divider } from "@mui/material";
 import { tagService } from "../services/TagService";
 import { Tag } from "../models/Tag";
-import { Sell as SellIcon, Stars } from "@mui/icons-material";
+import { issueService } from "../services/IssueService";
+import { Stars } from "@mui/icons-material";
+import { Issue } from "../models/Issue";
+import {
+  STATUS_ACCEPTED,
+  STATUS_COMPLETED,
+  STATUS_IN_PROGRESS,
+} from "../services/StatusService";
 
 const EpicsComponent: React.FC = () => {
   const [epics, setEpics] = useState<Tag[]>([]);
   const [filteredEpics, setFilteredEpics] = useState<Tag[]>([]);
   const [searchTerm, setSearchTerm] = useState<string>("");
+  const [issuesMap, setIssuesMap] = useState<Map<Tag, Issue[]>>(new Map());
 
   useEffect(() => {
     loadEpics();
     tagService.subscribeToGetAllTags(onEpicsUpdated);
+    issueService.subscribeToGetAllIssues(onEpicsUpdated);
     return () => {
       tagService.unsubscribeFromGetAllTags(onEpicsUpdated);
+      issueService.unsubscribeFromGetAllIssues(onEpicsUpdated);
     };
   }, []);
 
@@ -26,8 +36,13 @@ const EpicsComponent: React.FC = () => {
     const epicTags = fetchedEpics.filter((tag) => tag.isEpic);
     setEpics(epicTags);
     setFilteredEpics(epicTags);
+    const issuesPromises = epicTags.map((epic: Tag) =>
+      issueService.getIssuesByTag(epic.id).then((issues) => {
+        issuesMap.set(epic, issues);
+      }),
+    );
+    await Promise.all(issuesPromises);
   };
-
   useEffect(() => {
     const filtered = epics.filter((epic: Tag) =>
       epic.name.toLowerCase().includes(searchTerm.toLowerCase()),
@@ -36,7 +51,7 @@ const EpicsComponent: React.FC = () => {
   }, [searchTerm, epics]);
 
   return (
-    <Box sx={{ backgroundColor: "#f5f5f5" }}>
+    <Box sx={{ width: '100%', backgroundColor: "#e0e0f8" }}>
       <TextField
         fullWidth
         variant="outlined"
@@ -54,37 +69,100 @@ const EpicsComponent: React.FC = () => {
           },
         }}
       />
-      <Divider sx={{ bgcolor: "#666666" }} />
-      {filteredEpics.map((epic: Tag) => (
-        <React.Fragment key={epic.id}>
-          <Box
-            sx={{
-              display: "flex",
-              alignItems: "center",
-              p: 1,
-              "&:hover": { backgroundColor: "rgba(0, 0, 0, 0.04)" },
-            }}
-          >
-            <Stars sx={{ mr: 1, color: "#673ab7" }} />
-            <Typography
+      <Divider sx={{ bgcolor: "#666666", width: '100%' }} />
+      {Array.from(issuesMap.keys()).map((epic: any) => (
+        <Box key={epic.id} sx={{ width: '100%' }}>
+          <Box sx={{ width: '100%', p: 2 }}>
+            <ProgressBar issues={issuesMap.get(epic) || []} />
+            <Box
               sx={{
-                color: "#673ab7",
-                fontWeight: epic.count && epic.count > 0 ? "bold" : "normal",
-                flexGrow: 1,
-                cursor: "pointer",
-                "&:hover": {
-                  textDecoration: "underline",
-                },
+                display: "flex",
+                alignItems: "center",
+                width: '100%',
+                mt: 1,
+                "&:hover": { backgroundColor: "rgba(0, 0, 0, 0.04)" },
               }}
             >
-              {epic.name} ({epic.count || 0})
-            </Typography>
+              <Stars sx={{ mr: 1, color: "#673ab7" }} />
+              <Typography
+                sx={{
+                  color: "#673ab7",
+                  fontWeight: issuesMap.get(epic)?.length ? "bold" : "normal",
+                  cursor: "pointer",
+                  "&:hover": {
+                    textDecoration: "underline",
+                  },
+                }}
+              >
+                {epic.name} ({issuesMap.get(epic)?.length || 0})
+              </Typography>
+            </Box>
           </Box>
-          <Divider sx={{ bgcolor: "#666666" }} />
-        </React.Fragment>
+          <Divider sx={{ bgcolor: "#666666", width: '100%' }} />
+        </Box>
+      ))}
+    </Box>
+  );
+};const getStatusPercentage = (
+  issues: Issue[],
+  predicate: (issue: Issue) => boolean,
+): number => {
+  if (!issues?.length) return 0;
+  return (issues.filter(predicate).length / issues.length) * 100;
+};
+
+const ProgressBar: React.FC<{ issues: Issue[] }> = ({ issues }) => {
+  const statusBars = [
+    {
+      predicate: (issue: Issue) => issue.status === STATUS_IN_PROGRESS,
+      color: "#FFFFE0",
+    },
+    {
+      predicate: (issue: Issue) => issue.status === STATUS_COMPLETED,
+      color: "#c6d9b7",
+    },
+    {
+      predicate: (issue: Issue) => issue.status === STATUS_ACCEPTED,
+      color: "#a8c594",
+    },
+    {
+      predicate: (issue: Issue) => issue.isIcebox,
+      color: "#c9dff0",
+    },
+    {
+      predicate: (issue: Issue) => !issue.isIcebox && !issue.status,
+      color: "#f5f5f5",
+    },
+  ];
+
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        width: "100%",
+        height: "8px",
+        bgcolor: "#e0e0e0",
+        borderRadius: 1,
+        border: "1px solid darkgrey",
+      }}
+    >
+      {statusBars.map(({ predicate, color }, index) => (
+        <Box
+          key={index}
+          sx={{
+            width: `${getStatusPercentage(issues, predicate)}%`,
+            bgcolor: color,
+            height: "100%",
+            borderRadius:
+              index === 0
+                ? "4px 0 0 4px"
+                : index === statusBars.length - 1
+                  ? "0 4px 4px 0"
+                  : 0,
+          }}
+        />
       ))}
     </Box>
   );
 };
-
 export default EpicsComponent;
