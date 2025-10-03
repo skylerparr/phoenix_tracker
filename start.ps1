9ddcf7d2fba3111142a15ef8b8028b336728cdf7
Skyler Parrti <skylerparrti@gmail.com>
2025-10-03 11:27:58 -0700

    Update start script to specify file store environment settings

--- start.ps1
+++ start.ps1
@@ -6,12 +6,16 @@ $Env:COMPOSE_PROFILES="dev"
 $Env:DATABASE_URL="postgresql://postgres:postgres@postgres:5432/phoenix_tracker"
 $Env:FRONTEND_URL="http://localhost:4000"
 $Env:REACT_APP_API_BASE_URL="http://localhost:3001"
+$Env:FILE_STORE_SCHEME="local"
+$Env:BASE_FILE_PATH="/tmp"
 
 # Print environment variables for debugging
 Write-Host "Environment variables set:"
 Write-Host "REACT_APP_API_BASE_URL: $Env:REACT_APP_API_BASE_URL"
 Write-Host "DATABASE_URL: $Env:DATABASE_URL"
+Write-Host "FILE_STORE_SCHEME: $Env:FILE_STORE_SCHEME"
+Write-Host "BASE_FILE_PATH: $Env:BASE_FILE_PATH"
 
 # Run Docker Compose
 Write-Host "Starting Docker Compose..."
-docker-compose up 
+docker-compose up
